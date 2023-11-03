55825eca5b655ef80f32136acc42badc9d67af14
unknown <zarsement@hotmail.com>
2023-11-03 18:57:53 -0500

    Fixed bugs... SQUASHgit add *!

--- src/lib/resourcePhase/tileCard/TileCard.svelte
+++ src/lib/resourcePhase/tileCard/TileCard.svelte
@@ -1,38 +1,130 @@
 <script>
+    import { onMount } from "svelte";
+
     export let tileType;
     export let value;
     export let updateTileValue;
     export let government;
+    export let currentTiles;
+    export let currentToggleTiles;
+    export let toggle;
 
-    function updateValue() {
-        // if(tileType.includes("Dsrt")){
-        //     if (government == 'Monarchy Dst') {
-                
-        //     }
+    let monarchyType;
 
-        // } else if (government == 'Monarchy Swp' && tileType.includes("Swmp")) {
-            
-        // } else if (government == 'Monarchy Mtn' && tileType.includes("Mntn")) {
-            
-        // }
+    function updateValue(inputType, inputValue) {
+        let updateValue = removeLeadingZeros(inputValue);
 
-        let updateValue = removeLeadingZeros(value);
+        if(!inputValue || !updateValue){
+            updateValue = '0'
+        }
 
-        localStorage.setItem(tileType, updateValue)
-        updateTileValue(tileType, value);
+        updateTileValue(inputType, updateValue);
+        localStorage.setItem(inputType, updateValue)
+
+        if(inputType === "Desert"){
+            updateTileValue('DsrtGold', updateValue);
+            localStorage.setItem('DsrtGold', updateValue)
+            updateTileValue('DsrtTech', '0');
+            localStorage.setItem('DsrtTech', '0')
+        } else if(inputType === 'Mountain'){
+            updateTileValue('MntnIron', updateValue);
+            localStorage.setItem('MntnIron', updateValue)
+            updateTileValue('MntnGold', '0');
+            localStorage.setItem('MntnGold', '0')
+        } else if(inputType === 'Swamp'){
+            updateTileValue('SwmpFood', updateValue);
+            localStorage.setItem('SwmpFood', updateValue)
+            updateTileValue('SwmpWood', '0');
+            localStorage.setItem('SwmpWood', '0')
+        } else if(inputType === 'Ocean'){
+            updateTileValue('OcnFood', updateValue);
+            localStorage.setItem('OcnFood', updateValue)
+            updateTileValue('OcnTech', '0');
+            localStorage.setItem('OcnTech', '0')
+        }
     }
 
     function removeLeadingZeros(numberStr) {
         // Use regular expression to remove leading zeros
         return String(numberStr).replace(/^0+/, '');
     }
+
+    const addTileType = (inputType) => {
+        let parentTile;
+        let siblingTile;
+        let siblingCount;
+        let inputValue;
+
+        if(tileType.includes("Dsrt")){
+            parentTile = currentTiles.Desert;
+
+            if(tileType.includes("Gold")){
+                siblingTile = 'DsrtTech';
+            } else {
+                siblingTile = 'DsrtGold';
+            }
+        } else if(tileType.includes("Mntn")){
+            parentTile = currentTiles.Mountain;
+
+            if(tileType.includes("Gold")){
+                siblingTile = 'MntnIron';
+            } else {
+                siblingTile = 'MntnGold';
+            }
+        } else if(tileType.includes("Swmp")){
+            parentTile = currentTiles.Swamp;
+
+            if(tileType.includes("Food")){
+                siblingTile = 'SwmpWood';
+            } else {
+                siblingTile = 'SwmpFood';
+            }
+        } else if(tileType.includes("Ocn")){
+            parentTile = currentTiles.Ocean;
+
+            if(tileType.includes("Food")){
+                siblingTile = 'OcnTech';
+            } else {
+                siblingTile = 'OcnFood';
+            }
+        }
+
+        if(parseInt(currentToggleTiles[tileType]) < parseInt(parentTile)){
+            inputValue = String(parseInt(value) + 1);
+            updateValue(inputType, inputValue);
+
+            if(parseInt(currentToggleTiles[siblingTile]) > 0){
+                siblingCount =  String(parseInt(currentToggleTiles[siblingTile]) - 1)
+                updateValue(siblingTile, siblingCount);
+            }
+        }
+    }
+
+    onMount(() => {
+        monarchyType = null;
+
+        if(government.includes('Monarchy')){
+            if(government === 'Monarchy Dst' && tileType.includes("Dsrt")) {
+                monarchyType = 'D'
+            } else if (government === 'Monarchy Swp' && tileType.includes("Swmp")) {
+                monarchyType = 'S'
+            } else if (government === 'Monarchy Mtn' && tileType.includes("Mntn")) {
+                monarchyType = 'M'
+            }
+        }
+    })
 </script>
 <main>
     <div class='resource'>
         {tileType}
     </div>
 
-    <input class='value' bind:value={value} placeholder=0 type="number" on:change={() => updateValue()} />
+    {#if toggle==='true'}
+        <input class='value' bind:value={value} placeholder="" type="number" on:click={() => addTileType(tileType)} readonly/>
+    {:else}
+        <input class='value' bind:value={value} placeholder="" type="number" on:change={() => updateValue(tileType, value)}/>
+    {/if}
+
 
 </main>
 
